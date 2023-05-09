d5b077150d86949ac674303e7ef3cf6b781abd9c
@nataliia.vyrsta <natali.vyrsta@gmail.com>
2023-05-09 13:38:17 +0300

    first commit

---
+++ components/Hero.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <section className="hero container">
+      <div className="hero__content wrapper">
+        <h4 className="hero__subtitle">ASPIRE TO MORE</h4>
+        <h1 className="hero__title">
+          new
+          <span>collections</span>
+          <br />
+          by top
+          <span>brands</span>
+        </h1>
+        <a href="#" role="button" className="hero__btn">
+          Shop Now <span>⟶</span>
+        </a>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
