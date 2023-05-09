1ee606fa1e6c2152ba7e9fcb4524abf01653c755
@nataliia.vyrsta <natali.vyrsta@gmail.com>
2023-05-09 23:24:46 +0300

    add categories

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,9 +1,45 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Categories = () => {
   return (
-    <div>Categories</div>
-  )
+    <section className="categories container">
+      <div className="categories__content wrapper">
+        <div className="categories__item">
+          <Link
+            href="/category/new-arrivals"
+            className="categories__link"
+          ></Link>
+          <h2 className="categories__title"> new arrivals</h2>
+          <a href="#" className="categories__btn">
+            Shop Collection <span>⟶</span>
+          </a>
+        </div>
+
+        <div className="categories__item">
+          <Link
+            href="/category/trainers"
+            className="categories__link"
+          ></Link>
+          <h2 className="categories__title"> trainers</h2>
+          <a href="#" className="categories__btn">
+            Shop Collection <span>⟶</span>
+          </a>
+        </div>
+
+        <div className="categories__item">
+          <Link
+            href="/category/hot-sale"
+            className="categories__link"
+          ></Link>
+          <h2 className="categories__title"> hot sale</h2>
+          <a href="#" className="categories__btn">
+            Shop Collection <span>⟶</span>
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Categories;
